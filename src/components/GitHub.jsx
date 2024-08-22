ea20c5889f98e73d4abc2f675b37c1657af360cb
Willy Mendoza <willymendozaarias@gmail.com>
2024-08-22 00:43:32 -0400

    Init components

---
+++ src/components/GitHub.jsx
@@ -0,0 +1,16 @@
+const GitHUb = () => {
+    return (
+        <div>
+            <a
+                className="App-link"
+                href="https://github.com/WillyMendoza/desarrollo-frontend-react2024"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Mi proyecto react (GitHub)
+            </a>
+        </div>
+    );
+};
+
+export default GitHUb;
